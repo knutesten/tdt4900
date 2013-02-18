b68db2b6b299fd1a499d0c5db86ab935d3b55b67
Knut Esten Melandsø Nekså <knut.neksa@gmail.com>
2013-02-18 16:46:45 +0100

    Added stick.js that draws a "stick man graph"

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,9 +1,5 @@
-var test;
-
 $(function() {
-        parseActivPalData("../csv/dean.csv", function(data){
-                pie(data[1]);
-                clock(data[1]);
-                timeline(data[1]);
+        parseActivPalData("../csv/test2.csv", function(data){
+		stick(data[1]);
         });
 });
