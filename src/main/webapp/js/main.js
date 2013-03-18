9296e62681b730aececa110e94d4a280469a4fff
Knut Esten Melandsø Nekså <knut.neksa@gmail.com>
2013-03-18 18:09:24 +0100

    Menu is styled

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -11,10 +11,21 @@ function ViewModel(){
         {name: "Klokke (12 timer)", draw: clock12},
         {name: "Klokke (24 timer)", draw: clock24}
     ]);
-
+    for(var i = 0; i < self.charts().length; i++){
+        self.charts()[i].isSelected = ko.observable(false);
+    }
+    
     self.data = new Data();
     data = self.data;
-    self.drawGraph = function(d){
+
+    var prevSelected = undefined;
+    self.drawChart = function(d){
+        if(prevSelected){
+            prevSelected.isSelected(false);
+        }
+        prevSelected = d;
+        d.isSelected(true);
+
         $("#vis").empty();
         d.draw("#vis", self.data);
     }
